d3a67e55c11339231da70cc216f80af9e4d09d1b
AndresTCH22 <u202019858@upc.edu.pe>
2024-10-24 18:40:12 -0500

    graficas

--- src/public/js/resumen.js
+++ src/public/js/resumen.js
@@ -7,7 +7,6 @@ async function ListaIngresos() {
     const lista=await Ingreso.Lista();
     return FiltrarPorFecha(lista,año,mes);
 }
-
 async function ListaGastos() {
     const mes=parseInt(document.getElementById("mes").value);
     const año=parseInt(document.getElementById("año").value);
@@ -40,6 +39,15 @@ async function Resumen() {
     if(saldo_disponible<0){
         document.getElementById("saldo-disponible").style.color="#ff3333";
     }
+
+    ///graficas
+    const labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio','Agosto','Setiembre','Octubre','Noviembre','Diciembre'];
+    const ingresos_values = [3000, 4000, 3500, 5000, 4500,2000, 2500, 3000, 4000, 3500,2324,2135];
+    const gastos_values = [2000, 2500, 3000, 4000, 3500,3000, 4000, 3500, 5000, 4500,980,5200];
+
+    createPieChart('canvas_ingresos', ['Sueldo', 'Cachuelo'], [2500,200]);
+    createPieChart('canvas_gastos', ['Alimentacion', 'educacion',"servicios","servicios","otros"], [50,1250,200,200,500]);
+    createLineChart('canvas_anual', labels, ingresos_values, gastos_values);
 }
 
 
@@ -53,3 +61,7 @@ window.addEventListener("load",async ()=>{
     document.getElementById("mes").value=MesActual();
     await Resumen();
 })
+
+
+
+
